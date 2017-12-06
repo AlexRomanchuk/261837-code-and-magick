8e420492f28544b45293e134451b630423a14d0c
Alexander <borom@inbox.ru>
2017-12-06 18:21:02 +0300

    Перенесена функция валидация поля

--- js/modules/dialog.js
+++ js/modules/dialog.js
@@ -74,5 +74,26 @@
   });
 
   window.util.showBlock('.setup-similar');
-  window.util.validityForm();
+
+  (function () {
+    var userNameInput = window.mainSetup.userNameInput;
+    userNameInput.addEventListener('invalid', function () {
+      if (userNameInput.validity.tooShort) {
+        userNameInput.setCustomValidity('Имя должно содержать минимум 2 символа!');
+      } else if (userNameInput.validity.tooLong) {
+        userNameInput.setCustomValidity('Имя не должно превышать 25-ти символов!');
+      } else if (userNameInput.validity.valueMissing) {
+        userNameInput.setCustomValidity('Без имени не принимаем!');
+      } else {
+        userNameInput.setCustomValidity('');
+      }
+    });
+    userNameInput.addEventListener('input', function (evt) {
+      var target = evt.target;
+      var minSimbols = target.value.length;
+      var errMessage = '';
+      errMessage = minSimbols < 2 ? 'Имя должно содержать минимум 2 символа!' : '';
+      target.setCustomValidity(errMessage);
+    });
+  })();
 })();
