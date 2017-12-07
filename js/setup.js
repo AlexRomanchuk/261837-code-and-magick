3c11d70d2502ff39f1d3a9058efbca61b3dcd671
Alexander <borom@inbox.ru>
2017-12-07 11:55:34 +0300

    Начато создание настроек передвижения артефактов

--- js/setup.js
+++ js/setup.js
@@ -3,3 +3,33 @@
 var cellElement = document.querySelector('.setup-artifacts-cell');
 var shopElement = document.querySelector('.setup-artifacts-shop');
 var draggedItem = null;
+var cellsArtifacts = document.querySelector('.setup-artifacts');
+
+shopElement.addEventListener('dragstart', function (evt) {
+  if (evt.target.tagName.toLowerCase() === 'img') {
+    draggedItem = evt.target;
+    evt.dataTransfer.setData('text/plain', evt.target.alt);
+  }
+});
+
+cellsArtifacts.addEventListener('dragover', function (evt) {
+  evt.preventDefault();
+  return false;
+});
+
+cellsArtifacts.addEventListener('drop', function (evt) {
+  cellElement.style.backgroundColor = '';
+  evt.target.appendChild(draggedItem);
+  evt.preventDefault();
+});
+
+cellsArtifacts.addEventListener('dragenter', function (evt) {
+  cellElement.style.backgroundColor = 'yellow';
+  evt.preventDefault();
+});
+
+cellsArtifacts.addEventListener('dragleave', function (evt) {
+  cellElement.style.backgroundColor = '';
+  evt.preventDefault();
+});
+
