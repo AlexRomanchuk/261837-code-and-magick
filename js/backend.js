fb7e77142e218fba62b59cbc1eb14fdc1e889b66
Alexander <borom@inbox.ru>
2017-12-12 17:27:32 +0300

    Создан модуль backend

---
+++ js/backend.js
@@ -0,0 +1,24 @@
+﻿'use ctrict';
+widow.backend = (function () {
+  return {
+    load: function (onLoad, onError) {
+      var xhr = new XMLHttpRequest()
+      xhr.open(
+        'GET',
+        'https://1510.dump.academy/code-and-magick/data',
+        true
+      );
+      xhr.send()
+
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState !== 4) {
+          return 0;
+        }
+        console.log('end')
+      }
+    },
+    save: function (data, onLoad, onError) {
+
+    }
+  }
+})();
