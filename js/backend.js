13c0f0e4dc8f28585249129d48c01768d8720ee6
Alexander <borom@inbox.ru>
2017-12-14 10:17:06 +0300

    Создана функция логики xhr

--- js/backend.js
+++ js/backend.js
@@ -3,64 +3,55 @@
 window.backend = (function () {
   var url = 'https://1510.dump.academy/code-and-magick';
   var errorStyle = 'background: red; border: 5px dashed white';
-  var emergencyStyle = 'color: black; background: "#e5be01"; border: 5px dashed black';
+  var emergencyStyle = 'color: black; background: #e5be01; border: 5px dashed black';
+
+  window.messageError = function (message, alertStyle) {
+    var header = document.querySelector('.header-clouds');
+    var errorAlert = document.createElement('div');
+    errorAlert.style = alertStyle;
+    errorAlert.textContent = message;
+    header.appendChild(errorAlert);
+  };
+
+  function actionXhr(onLoad, onError, method, link, data) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.timeout = 15000;
+
+    xhr.addEventListener('load', function () {
+      switch (xhr.status) {
+        case 200:
+          onLoad(xhr.response);
+          break;
+        case 404:
+          onError('Мы не нашли url ' + url + '... Ошибка ' + xhr.status + ' ' + xhr.statusText, errorStyle);
+          break;
+        default:
+          onError('Какая-то авария... Неизвестная ошибка: ' + xhr.status + ' ' + xhr.statusText, errorStyle);
+          break;
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('Авария... Произошла ошибка соединения', emergencyStyle);
+    });
+
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс', emergencyStyle);
+    });
+
+    xhr.open(method, url + link);
+
+    xhr.send(data);
+  }
+
   return {
     load: function (onLoad, onError) {
-      var xhr = new XMLHttpRequest();
-      xhr.responseType = 'json';
-      xhr.timeout = 15000;
-
-      xhr.addEventListener('load', function () {
-        switch (xhr.status) {
-          case 200:
-            onLoad(xhr.response);
-            break;
-          case 404:
-            onError('Мы не нашли данных по url ' + url + '... Ошибка ' + xhr.status + ' ' + xhr.statusText, errorStyle);
-            break;
-          default:
-            onError('Какая-то авария... Неизвестная ошибка: ' + xhr.status + ' ' + xhr.statusText, errorStyle);
-            break;
-        }
-      });
-
-      xhr.addEventListener('error', function () {
-        onError('Авария... Произошла ошибка соединения', emergencyStyle);
-      });
-
-      xhr.addEventListener('timeout', function () {
-        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс', emergencyStyle);
-      });
-
-      xhr.open('GET', url + '/data');
-
-      xhr.send();
+      actionXhr(onLoad, onError, 'GET', '/data');
     },
 
-    save: function (data, onLoad, onError) {
-      var xhr = new XMLHttpRequest();
-      xhr.responseType = 'json';
-      xhr.addEventListener('load', function () {
-        switch (xhr.status) {
-          case 200:
-            onLoad(xhr.response);
-            break;
-          default:
-            onError('Какая-то авария... Неизвестная ошибка: ' + xhr.status + ' ' + xhr.statusText, errorStyle);
-            break;
-        }
-      });
-
-      xhr.addEventListener('error', function () {
-        onError('Авария... Произошла ошибка соединения', emergencyStyle);
-      });
-
-      xhr.addEventListener('timeout', function () {
-        onError('Не удалось отправить форму за ' + xhr.timeout + 'мс', emergencyStyle);
-      });
-
-      xhr.open('POST', url);
-      xhr.send(data);
+    save: function (formdata, onLoad, onError) {
+      actionXhr(onLoad, onError, 'POST', '', formdata);
     }
   };
 })();
--- js/modules/renderWizards.js
+++ js/modules/renderWizards.js
@@ -2,14 +2,6 @@
 
 (function () {
   var similarListElement = window.mainSetup.setup.querySelector('.setup-similar-list');
-  var header = document.querySelector('.header-clouds');
-
-  window.messageError = function (message, alertStyle) {
-    var errorAlert = document.createElement('div');
-    errorAlert.style = alertStyle;
-    errorAlert.textContent = message;
-    header.appendChild(errorAlert);
-  };
 
   window.backend.load(function (wizards) {
     var fragment = document.createDocumentFragment();
