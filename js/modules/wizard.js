66f7f5bb06f65a415f04d155b1bd3848e193e2d2
Alexander <borom@inbox.ru>
2017-12-15 13:25:44 +0300

    Создан модуль отрисовки волшебника

---
+++ js/modules/wizard.js
@@ -0,0 +1,21 @@
+﻿'use strict';
+(function () {
+  window.wizard = {
+    onEyesChange: function (elem, color) {},
+    onCoatChange: function (elem, color) {}
+  }
+
+  window.mainSetup.coat.addEventListener('click', function () {
+    var newColor = window.util.getRandData(window.mainSetup.coatColors);
+    this.style.fill = newColor;
+    coatColor = newColor;
+    updateWizards();
+  });
+
+  window.mainSetup.eyes.addEventListener('click', function () {
+    var newColor = window.util.getRandData(window.mainSetup.eyesColors);
+    this.style.fill = newColor;
+    eyesColor = newColor;
+    updateWizards();
+  });
+})();
